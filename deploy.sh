f22bf859af610a739c51c42c690d16d25be2201c
S0UL-R3AP3R <rober116@hotmail.com>
2019-07-01 04:07:01 -0500

    fixing build

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,4 @@
 !/usr/bin/env bash
-########################################
-# Put this on a Server
-# run chmod +x deploy_app.sh to make the script executable
-#
-# Execute this script:  ./deploy_app.sh ariv3ra/python-circleci-docker:$TAG
-# Replace the $TAG with the actual Build Tag you want to deploy
-#
-########################################
 
 set -e
 
