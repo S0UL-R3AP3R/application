07c22991aae6b5d5a01934d924a5ab3fef6d0b46
S0UL-R3AP3R <rober116@hotmail.com>
2019-07-02 01:43:16 -0500

    cleaning up deploy

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,8 @@ set -e
 DOCKER_IMAGE="timeoff:latest"
 CONTAINER_NAME="alpine_timeoff"
 
-#sudo apt install -y httpd
+sudo docker load -i /my-app-docker.tar.gz
+#sudo apt-get install -y httpd
 
 # Check for existing image
 if [[ "$(sudo docker images -q $DOCKER_IMAGE 2> /dev/null)" == "" ]]; then
