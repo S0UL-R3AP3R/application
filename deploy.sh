b123f70be95707c53232a46106cc7acf85798266
S0UL-R3AP3R <rober116@hotmail.com>
2019-07-01 19:34:08 -0500

    cleaning up build

--- deploy.sh
+++ deploy.sh
@@ -2,24 +2,16 @@
 
 set -e
 
-DOCKER_IMAGE="timeoff:latest"
 CONTAINER_NAME="alpine_timeoff"
-
-# Check for arguments
-if [[ $# -lt 1 ]] ; then
-    sudo docker build --rm -t timeoff:latest --no-cache .
-    exit 1
-fi
-
-echo "Deploying App to Docker Container"
 
 #Check for running container & stop it before starting a new one
 if [[ $(docker inspect -f '{{.State.Running}}' ${CONTAINER_NAME}) = "true" ]]; then
     sudo docker stop alpine_timeoff
 fi
 
-echo "Starting App using Docker Image name: $DOCKER_IMAGE"
-
+echo "Building Image"
+sudo docker build --rm -t timeoff:latest --no-cache .
+echo "Running Docker Container using Docker Image name: timeoff:latest"
 sudo docker run -t -i -d -p 3000:3000 --name alpine_timeoff timeoff
 
 sudo docker ps -a
