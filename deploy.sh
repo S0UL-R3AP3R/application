26c2d21f635d94e3e30e34dbeb8a93786dcd665b
S0UL-R3AP3R <rober116@hotmail.com>
2019-07-04 19:03:06 -0500

    fixing bugs

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd /var/www/
+
 set -e
 
 DOCKER_IMAGE="timeoff:latest"
@@ -7,23 +9,18 @@ CONTAINER_NAME="alpine_timeoff"
 
 # Check for existing image
 if [[ "$(sudo docker images -q $DOCKER_IMAGE 2> /dev/null)" == "" ]]; then
-            echo 'Building Image:'
-            sudo docker build --rm -t timeoff:latest --no-cache .
-                exit 1
-fi
-
-#Check for running container & stop it before starting a new one
-if [ "$(sudo docker ps -aq -f status=running -f name=${CONTAINER_NAME})" ]; then
-       echo "Container already exist, cleaning up"
-        sudo docker stop $(sudo docker ps -aqf "name=${CONTAINER_NAME}")
-
-        if [ "$(sudo docker ps -aq -f status=exited -f name=${CONTAINER_NAME})" ]; then
-                echo "Removing Existing Container"
-                sudo docker rm $(sudo docker ps -aqf "name=${CONTAINER_NAME}")
-
-        fi
-
-#        echo "Starting App using Docker Image name: $CONTAINER_NAME"
+        echo 'Building Image:'
+        sudo docker build --rm -t timeoff:latest --no-cache .
+        echo "Starting App using Docker Image name: $CONTAINER_NAME"
         sudo docker run -t -i -d -p 3000:3000 --name alpine_timeoff timeoff
         sudo docker ps -a
 fi
+
+sudo docker stop $(sudo docker ps -aqf "name=${CONTAINER_NAME}")
+sudo docker rm $(sudo docker ps -aqf "name=${CONTAINER_NAME}")
+sudo docker rmi -f $(sudo docker images --filter=reference=timeoff --format "{{.ID}}") $(sudo docker images --filter=reference=alpine --format "{{.ID}}")
+sudo docker build --rm -t timeoff:latest --no-cache .
+
+echo "Starting App using Docker Image name: $CONTAINER_NAME"
+sudo docker run -t -i -d -p 3000:3000 --name alpine_timeoff timeoff
+sudo docker ps -a
